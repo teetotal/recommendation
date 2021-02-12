22c5bc858cbd6b2b613a618e637ca8f94e9d3414
Jung <daejung@APSEONOTE467.ad.ea.com>
2021-02-12 15:57:38 +0900

    init

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
-SELECT A.aid AS user, A.spid * 10 + A.grade AS item, A.rating /B.max_rating * 5 AS rating
+SELECT A.aid AS user, A.spid * 10 + A.grade AS item, A.rating / B.max_rating * 5 AS rating
 FROM D20210128_CF_COUNT A
 	INNER JOIN D20210128_CF_MAX B
 		ON A.aid = B.aid 
-WHERE B.max_rating > 9 
+	INNER JOIN D20210128_CLUSTER_RESULT C
+		ON A.aid = C.aid 
+WHERE C.`cluster` = 6
