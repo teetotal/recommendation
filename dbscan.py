dbe34a2d93501f92fa4719ddc67091fd6a8ee642
Jung <daejung@APSEONOTE467.ad.ea.com>
2021-03-10 15:04:29 +0900

    init

--- dbscan.py
+++ dbscan.py
@@ -1,24 +1,26 @@
 from util import load_data_cluster
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 from h5 import *
 
-#const
-clusters = 16
-max_iter = 3000
+#const. default로 설정
+eps = 0.5
+min_samples = 5
+
+
 print("Loading data")
 aids, ratings = load_data_cluster('data_cluster/train.h5')
 
-print("Starting K-means")
-km = KMeans(n_clusters=clusters, max_iter=max_iter).fit(ratings)
+print("Starting DBSCAN")
+dbscan = DBSCAN(eps=eps, min_samples=min_samples).fit(ratings) 
 
 print("Saving Result")
-result_file_path = 'kmeans_result.h5'
+result_file_path = 'dbscan_result.h5'
 h5 = h5_gen(result_file_path, 'gzip') 
 
-for n in range(len(km.labels_)):
+for n in range(len(dbscan.labels_)):
     aid = aids[n]
-    cluster = [int(km.labels_[n])]
+    cluster = [int(dbscan.labels_[n])]
     h5.write('/cluster', aid, cluster)
 h5.close()
 print("All Done.", result_file_path)
